94316c4e5b7e72e89f7421316ebceada91b8c824
GuidoCostaDF <guidocostadf@gmail.com>
2022-09-02 12:28:49 -0300

    implementando logica do teclado

--- src/app/shared/directives/keyboard-manager/keyboard-manager.module.ts
+++ src/app/shared/directives/keyboard-manager/keyboard-manager.module.ts
@@ -1,10 +1,11 @@
 import { KeyboardManagerDirective } from './keyboard-manager.directive';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { KeyboardManagedItemDirective } from './keyboard-managed-item.directive';
 
 @NgModule({
-  declarations: [KeyboardManagerDirective],
+  declarations: [KeyboardManagerDirective, KeyboardManagedItemDirective],
   imports: [CommonModule],
-  exports: [KeyboardManagerDirective],
+  exports: [KeyboardManagerDirective, KeyboardManagedItemDirective],
 })
 export class KeyboardManagerModule {}
